4ac968073f5b7beea1c2cf78094cafe4fb6cb3db
Max Makaluk <70341920+FussuChalice@users.noreply.github.com>
2023-08-08 21:17:34 +0300

    Update HarterHatewayFractal.h

--- HarterHatewayFractal.h
+++ HarterHatewayFractal.h
@@ -3,6 +3,11 @@
 
 #pragma once
 
+#include <SFML/Graphics.hpp>
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <fstream>
+#include <vector>
 
 // TODO: Reference additional headers your program requires here.
